ad69d9c1da30c6a94f8e84c48976df5032a8805e
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-12-27 00:09:57 -0800

    0.0.11 use importlib, same result

--- pack/main.py
+++ pack/main.py
@@ -1,11 +1,11 @@
-import os, sys
+import importlib, os, sys
 
 def local_import(argv=None):
-    print("0.0.10")
+    print("0.0.11")
     if argv is None:
         argv = sys.argv
     
     currentPath = os.getcwd()
     if currentPath not in sys.path:
         sys.path.append(currentPath)
-    print(__import__(argv[1]))
+    print(importlib.import_module(argv[1]))
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as 自述文件:
 
 setuptools.setup(
     name="test-package-name",
-    version="0.0.10",
+    version="0.0.11",
     author="小名",
     author_email="author@example.com",
     description="描述",
