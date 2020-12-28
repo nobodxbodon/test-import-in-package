224c4333c53ad568449715b32ab3a0dc74b60fcc
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-12-27 22:04:03 -0800

    Revert "0.0.14 add current path to sys.path"
    This reverts commit e3ab57ca47281f4dea8ab35886f074cdea4544f4.

--- pack/main.py
+++ pack/main.py
@@ -1,13 +1,6 @@
 import importlib, os, sys
 
 def local_import(argv=None):
-    print("0.0.14")
-
-    currentPath = os.getcwd()
-    if currentPath not in sys.path:
-        sys.path.append(currentPath)
-    else:
-        print("current path in sys.path already!")
-
+    print("0.0.13")
     print(sys.path)
     print(importlib.import_module(argv[1] if argv else sys.argv[1]))
