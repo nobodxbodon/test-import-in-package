e3ab57ca47281f4dea8ab35886f074cdea4544f4
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-12-27 19:31:03 -0800

    0.0.14 add current path to sys.path

--- pack/main.py
+++ pack/main.py
@@ -1,6 +1,13 @@
 import importlib, os, sys
 
 def local_import(argv=None):
-    print("0.0.13")
+    print("0.0.14")
+
+    currentPath = os.getcwd()
+    if currentPath not in sys.path:
+        sys.path.append(currentPath)
+    else:
+        print("current path in sys.path already!")
+
     print(sys.path)
     print(importlib.import_module(argv[1] if argv else sys.argv[1]))
